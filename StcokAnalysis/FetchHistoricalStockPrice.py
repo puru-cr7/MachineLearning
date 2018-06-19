333f25c5e3927acd805056e95e2a84f1eea2686f
Purnendu Rath (puru_cr7) <Purnendu Rath (puru_cr7)@DESKTOP-SMOVB8O>
2018-06-20 00:02:35 +0530

    commiting first few files

---
+++ StcokAnalysis/FetchHistoricalStockPrice.py
@@ -0,0 +1,16 @@
+'''
+Created on Jun 19, 2018
+
+@author: Purnendu Rath (puru_cr7)
+'''
+
+import fix_yahoo_finance as yf
+
+
+# This method downloads a list of stoock historical prices 
+# and stores as csv files is mentioned directory
+def downloadStockData(syms, start_date, end_date, direc):
+    for sym in syms:
+        data = yf.download(sym, start_date, end_date)
+        data.to_csv(direc + sym + ".csv")
+
